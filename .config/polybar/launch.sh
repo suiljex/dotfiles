be3af0456d50501b042224bfee5f1ff103d044d4
Igor G <account@suiljex.space>
2021-12-16 10:44:44 +0000

    change audio module for polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,8 +9,8 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+# Launch bar "main"
+echo "---" | tee -a /tmp/polybar.log
+polybar main 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Bars launched..."
