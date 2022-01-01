627f78ebfbf0763e8a3259b557d6fcf445296803
Igor G <account@suiljex.space>
2022-01-01 23:28:51 +0700

    fix plugins' config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,4 +1,4 @@
--- Automatically bootstrap packer.nvi
+-- Automatically bootstrap packer.nvim
 local exec = vim.api.nvim_command
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
@@ -7,14 +7,6 @@ if fn.empty(fn.glob(install_path)) > 0 then
   exec 'packadd packer.nvim'
 end
 
--- Disable nvim-tree icons
-vim.g.nvim_tree_show_icons = {
-  folders = 0,
-  files = 0,
-  git = 0,
-  folder_arrows = 0,
-}
-
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -22,7 +14,16 @@ return require('packer').startup(function(use)
   use 'vim-airline/vim-airline'
   use {
     'kyazdani42/nvim-tree.lua',
-    config = function() require'nvim-tree'.setup {} end
+    setup = function() 
+      -- Disable nvim-tree icons
+      vim.g.nvim_tree_show_icons = {
+        folders = 0,
+        files = 0,
+        git = 0,
+        folder_arrows = 0,
+      }
+    end,
+    config = function() require'nvim-tree'.setup {} end,
   }
   use 'ap/vim-css-color'
 
