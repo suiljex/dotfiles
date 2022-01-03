791be686389d841d8f8ef731b81f990cac0b75df
Igor G <account@suiljex.space>
2022-01-03 17:45:37 +0700

    fix pink color of default colorscheme

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -2,6 +2,10 @@ local colorscheme = "darkplus"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
+  vim.cmd [[highlight Pmenu ctermbg=gray guibg=gray]]
+  vim.cmd [[highlight PmenuSel ctermbg=gray guibg=gray]]
+  vim.cmd [[highlight PmenuSbar ctermbg=gray guibg=gray]]
+  vim.cmd [[highlight PmenuThumb ctermbg=gray guibg=gray]]
   vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
