6492e050af41e13de1e199e5dc71227ed4a70e6c
Igor G <account@suiljex.space>
2022-01-03 14:58:46 +0700

    fix telescope config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,9 @@ end
 
 telescope.setup {
   defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+
     vimgrep_arguments = {
       'rg',
       '--color=never',
@@ -16,10 +19,33 @@ telescope.setup {
       '--unrestricted',
       '--unrestricted',
     },
+
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        --["<C-h>"] = "which_key"
+      }
+    }
   },
   pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+
     find_files = {
       hidden = true,
     },
   },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+  }
 }
