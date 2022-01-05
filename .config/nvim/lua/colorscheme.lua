e9b2d21e27aca7c5cc59c0e7dac57431d6c349c2
Igor G <account@suiljex.space>
2022-01-06 00:56:13 +0700

    fix default colors

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -2,8 +2,8 @@ local colorscheme = "darkplus"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  vim.cmd [[highlight Pmenu ctermbg=gray guibg=gray]]
-  vim.cmd [[highlight PmenuSel ctermbg=gray guibg=gray]]
+  vim.cmd [[highlight Pmenu ctermbg=darkgrey guibg=darkgrey]]
+  vim.cmd [[highlight PmenuSel ctermbg=grey guibg=grey]]
   vim.cmd [[highlight PmenuSbar ctermbg=gray guibg=gray]]
   vim.cmd [[highlight PmenuThumb ctermbg=gray guibg=gray]]
   vim.notify("colorscheme " .. colorscheme .. " not found!")
