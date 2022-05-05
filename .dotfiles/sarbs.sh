b38b81f850413e3fed1109bde6f77aa445d88e79
suiljex <git@suiljex.space>
2022-05-05 11:21:07 +0700

    fix sed regex

--- .dotfiles/sarbs.sh
+++ .dotfiles/sarbs.sh
@@ -203,11 +203,11 @@ newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
 
 # Make pacman colorful, concurrent downloads and Pacman eye-candy.
 #grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
-sed -i "s/^#ParallelDownloads.*$/ParallelDownloads = 5/" /etc/pacman.conf
+sed -i "/^#ParallelDownloads/s/=.*/= 5/" /etc/pacman.conf
 sed -i "s/^#Color$/Color/" /etc/pacman.conf
 
 # Use all cores for compilation.
-sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
+sed -i "s/-j2/-j$(nproc)/;/^#MAKEFLAGS/s/^#//" /etc/makepkg.conf
 
 manualinstall yay-bin || error "Failed to install AUR helper."
 
